faac81a45a3c3c047adb09abc482cb460bc8ff0a
cdhbze93 <antoebt@hotmail.com>
2020-01-29 14:29:57 +0100

    build: before edit homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoadingComponent } from './pages/loading/loading.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoadingComponent
+    LoadingComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/loading/loading.component.ts
+++ src/app/pages/loading/loading.component.ts
@@ -16,6 +16,7 @@ export class LoadingComponent implements OnInit {
     this.mouseMove();
   }
 
+
   public countFunc() {
     const countHTML = document.getElementById('count');
 
@@ -25,6 +26,7 @@ export class LoadingComponent implements OnInit {
         countHTML.style.opacity = '0';
         setTimeout(() => { // Display le triangle après 1s
           document.getElementById('triangle-code').style.opacity = '1';
+          document.getElementById('cursor').style.opacity = '1';
         }, 1000);
       } else {
         this.count--; // Réduit le compteur de 1
@@ -43,7 +45,20 @@ export class LoadingComponent implements OnInit {
     });
   }
 
-  public click() {
-    document.getElementById('triangle-code').style.animation = 'leave linear 1.5s forwards';
+  public triangleClick() {
+    const triangleCSS = document.getElementById('triangle-code').style;
+    const flashCSS = document.getElementById('flash').style;
+    const timeBeforeFlash = 1500;
+
+    triangleCSS.animation = 'leave linear 1.5s forwards';
+
+    setTimeout( () => {
+      flashCSS.opacity = '1';
+      flashCSS.transform = 'scale(4)';
+    }, timeBeforeFlash );
+
+    setTimeout( () => {
+      flashCSS.animation = 'fadeOut linear 5s 2s forwards';
+    }, timeBeforeFlash + 2000);
   }
 }
